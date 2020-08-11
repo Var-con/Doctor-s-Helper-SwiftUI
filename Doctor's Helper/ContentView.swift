b01f49a6109eda57960ed1773da6ca08985a6fa7
Stanislav Klimov <kpuctallaz@gmail.com>
2020-08-11 18:20:48 +0500

    Initial Commit

---
+++ Doctor's Helper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Doctor's Helper
+//
+//  Created by Станислав Климов on 11.08.2020.
+//  Copyright © 2020 Stanislav Klimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
