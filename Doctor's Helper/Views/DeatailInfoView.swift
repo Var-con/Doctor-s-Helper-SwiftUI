22eb7df871aeb0135b48711569b081f753118484
Stanislav Klimov <kpuctallaz@gmail.com>
2020-08-25 15:24:27 +0500

    version 8.2 with extended

--- Doctor's Helper/Views/DeatailInfoView.swift
+++ Doctor's Helper/Views/DeatailInfoView.swift
@@ -25,65 +25,7 @@ struct DeatailInfoView: View {
         ScrollView {
             VStack {
                 Spacer()
-                VStack {
-                    VStack {
-                        Text("Лист нетрудоспособности: № ")
-                        Text("\(list.listNumber)")
-                            .fontWeight(.bold)
-                    }
-                    HStack {
-                        Text("Дата начала нетрудоспособности: ")
-                        Spacer()
-                        Text(DateFormatter.localizedString(from: list.startDate,
-                                                           dateStyle: .medium,
-                                                           timeStyle: .none))
-                            .fontWeight(.bold)
-                    }
-                    HStack {
-                        Text("Дата окончания нетрудоспособности: ")
-                        Spacer()
-                        Text(DateFormatter.localizedString(from: list.endDate,
-                                                           dateStyle: .medium,
-                                                           timeStyle: .none))
-                            .fontWeight(.bold)
-                    }
-                    HStack {
-                        if stringsCount.count < 2 {
-                        Button(action: {
-                            self.continueIsPresented.toggle()
-                        }) {
-                            Text("Продлить")
-                        }.sheet(isPresented: $continueIsPresented) {
-                            ContinueWithStringCalculate(list: self.list,
-                                                        showModal: self.$continueIsPresented,
-                                                        date: self.list.endDate)
-                        }
-                        }
-                        
-                        
-                        Spacer()
-                        Button( action: {
-                            self.showAskingOfDeleteAlert.toggle()
-                        }) {
-                            Text("Удалить").fontWeight(.bold)
-                        }
-                        .frame(width: 100, height: 30)
-                        .modifier(DeleteButtonModifier())
-                    }
-                    .alert(isPresented: self.$showAskingOfDeleteAlert) {
-                        Alert(title: Text("Вы точно хотите удалить л/н?"),
-                              primaryButton: Alert.Button.destructive(Text("Да"), action: {
-                                self.deleteList(list: self.list)}),
-                              secondaryButton: Alert.Button.cancel())
-                    }
-                }
-                .onAppear {
-                        self.stringsCount = self.lists.fetchContinueStrings(with: self.list.listNumber)
-                    }
-                .padding()
-                .modifier(SectionModifier())
-                .padding(.top, 20)
-                
+                SectionView(list: list, continueIsPresented: continueIsPresented, isActive: $isActive)
                 Spacer()
                 VStack {
                     ForEach(lists.fetchListWithPrevioslyNumber()) { nextList in
@@ -199,3 +141,77 @@ extension DeatailInfoView {
     }
  
 }
+
+//struct SectionView: View {
+//    
+//    @State var list: ListOfUnworking
+//    @State var continueIsPresented: Bool
+//    @State var lists: [ContinueListWithoutNumber] = []
+//    @State var showAskingOfDeleteAlert: Bool = false
+//    @State var action: () -> Void
+//    var body: some View {
+//        
+//        VStack {
+//            VStack {
+//                Text("Лист нетрудоспособности: № ")
+//                Text("\(list.listNumber)")
+//                    .fontWeight(.bold)
+//            }
+//            HStack {
+//                Text("Дата начала нетрудоспособности: ")
+//                Spacer()
+//                Text(DateFormatter.localizedString(from: list.startDate,
+//                                                   dateStyle: .medium,
+//                                                   timeStyle: .none))
+//                    .fontWeight(.bold)
+//            }
+//            HStack {
+//                Text("Дата окончания нетрудоспособности: ")
+//                Spacer()
+//                Text(DateFormatter.localizedString(from: list.endDate,
+//                                                   dateStyle: .medium,
+//                                                   timeStyle: .none))
+//                    .fontWeight(.bold)
+//            }
+//            HStack {
+//                if lists.count < 2 {
+//                    Button(action: {
+//                        self.continueIsPresented.toggle()
+//                    }) {
+//                        Text("Продлить")
+//                    }.sheet(isPresented: $continueIsPresented) {
+//                        ContinueWithStringCalculate(list: self.list,
+//                                                    showModal: self.$continueIsPresented,
+//                                                    date: self.list.endDate)
+//                    }
+//                }
+//                
+//                
+//                Spacer()
+//                Button( action: {
+//                    self.showAskingOfDeleteAlert.toggle()
+//                }) {
+//                    Text("Удалить").fontWeight(.bold)
+//                }
+//                .frame(width: 100, height: 30)
+//                .modifier(DeleteButtonModifier())
+//            }
+//            .alert(isPresented: self.$showAskingOfDeleteAlert) {
+//                Alert(title: Text("Вы точно хотите удалить л/н?"),
+//                      primaryButton: Alert.Button.destructive(Text("Да"), action: {
+//                        self.action()
+//                      }),
+//                      secondaryButton: Alert.Button.cancel())
+//            }
+//        }
+//        .onAppear {
+////            self.stringsCount = self.$lists.fetchContinueStrings(with: self.list.listNumber)
+//        }
+//        .padding()
+//        .modifier(SectionModifier())
+//        .padding(.top, 20)
+//    }
+//}
+
+// добавить экшен и вывесты блок л/н в отдельный файл
+
